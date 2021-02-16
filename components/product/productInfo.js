a96d641afb5f01ddfafeb32286230ee24fb80593
marcellmueller <mlmueller@protonmail.com>
2021-02-15 17:00:45 -0800

    Fixed product modal bug

--- components/product/productInfo.js
+++ components/product/productInfo.js
@@ -15,9 +15,7 @@ export default function ProductInfo(props) {
         <div className="flex items-center relative select-none">
           <div className="m-10 p-2 fixed left-10 right-10 top-0 w-30 mx-auto bg-white w-1/2  sm:min-w-md md:max-w-sm  rounded shadow-lg z-50">
             <hr />
-          </div>
-          <div className="w-100 h-100">
-            <LoginForm></LoginForm>
+            Product Name
           </div>
           <div
             onClick={hide}
